8666568b85787d36eda7c0147af50bbb840bece5
vcspc <vitonilcius@gmail.com>
2024-09-04 20:32:52 -0300

    remoção da imagem de banner e inserção do código do google analytics

--- landing-page-valadares/src/app/layout.jsx
+++ landing-page-valadares/src/app/layout.jsx
@@ -1,13 +1,12 @@
 import { Inter } from "next/font/google";
-/* import "style/globals.css"; */
 import "./globals.scss";
-
 import Link from "next/link";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Valadares Fueta Advogaodos",
+  title: "Valadares Fueta Advogados",
   description: "Serviço de ",
   keywords: "Serviços jurídicos para empresas, assessoria jurídica para agências de publicidade, serviços jurídicos para e-commerce, advogado para agências de publicidade, materiais jurídicos para agências de marketing, direito empresarial, direito digital, marketing digital",
   icons: {
@@ -18,11 +17,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FZZYVKNC6D"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FZZYVKNC6D');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <header>
-          <img src="/banner.svg" alt="" className="header__img"/>
+          <img src="/fundo_branco.svg" alt="" className="header__img" />
           <div className="header__banner">
-            <img src="/logo.svg" alt="logo Valadares Fueta Advogaodos" className="header__banner__logo"  />
+            <img src="/logo.svg" alt="logo Valadares Fueta Advogados" className="header__banner__logo" />
             <div className="header__banner__texto">
               <p className="header__banner__texto__1">Sua empresa exige um</p>
               <p className="header__banner__texto__2">PARCEIRO JURÍDICO QUE ENTENDA SEUS DESAFIOS.</p>
@@ -38,9 +52,15 @@ export default function RootLayout({ children }) {
               <div className="footer__container__esquerdo__acima">
                 <img src="/logo_branco.svg" alt="" />
                 <div className="footer__container__esquerdo__acima__termos">
-                  <Link style={{textdecoration: "none", color: "inherit"}} href="/termodeprivacidade"><p>Termo de Privacidade</p></Link>
-                  <Link style={{textdecoration: "none", color: "inherit"}} href="/termodeuso"><p>Termos de Uso</p></Link>
-                  <Link style={{textdecoration: "none", color: "inherit"}} href="/politicadecookies"><p>Política de Cookies</p></Link>
+                  <Link style={{ textDecoration: "none", color: "inherit" }} href="/termodeprivacidade">
+                    <p>Termo de Privacidade</p>
+                  </Link>
+                  <Link style={{ textDecoration: "none", color: "inherit" }} href="/termodeuso">
+                    <p>Termos de Uso</p>
+                  </Link>
+                  <Link style={{ textDecoration: "none", color: "inherit" }} href="/politicadecookies">
+                    <p>Política de Cookies</p>
+                  </Link>
                 </div>
               </div>
               <ul className="footer__container__esquerdo__abaixo">
@@ -54,12 +74,18 @@ export default function RootLayout({ children }) {
                 <li>marketing digital</li>
               </ul>
             </div>
-            <dir className="footer__container__direito">
+            <div className="footer__container__direito">
               <p className="footer__container__direito__texto">Siga nas redes</p>
               <div className="footer__container__direito__redes">
-                <Link href="https://www.instagram.com/valadaresefueta.adv?igsh=MmZhZmx5b2Nibnly"><img src="instagram.svg" alt="" /></Link>
-                <Link href="https://www.facebook.com/ValadareseFueta?mibextid=ZbWKwL"><img src="facebook.svg" alt="" /></Link>
-                <Link href="https://www.linkedin.com/company/valadares-e-fueta-advogados/"><img src="linkedin.svg" alt="" /></Link>
+                <Link href="https://www.instagram.com/valadaresefueta.adv?igsh=MmZhZmx5b2Nibnly">
+                  <img src="instagram.svg" alt="" />
+                </Link>
+                <Link href="https://www.facebook.com/ValadareseFueta?mibextid=ZbWKwL">
+                  <img src="facebook.svg" alt="" />
+                </Link>
+                <Link href="https://www.linkedin.com/company/valadares-e-fueta-advogados/">
+                  <img src="linkedin.svg" alt="" />
+                </Link>
               </div>
               <p className="footer__container__direito__texto">Entre em contato</p>
               <div className="footer__container__direito__whatsapp">
@@ -77,14 +103,15 @@ export default function RootLayout({ children }) {
                 </div>
               </div>
               <p className="footer__container__direito__texto">Atendemos em todo Brasil.</p>
-            </dir>
+            </div>
           </div>
           <div className="footer__container__copyright">
-            <p>© Copyright 2024 – Todos os Direitos Reservados <span>VALADARES E FUETA ADVOGADOS</span> Desenvolvido por <span>VIRA NEGÓCIOS</span></p>
+            <p>
+              © Copyright 2024 – Todos os Direitos Reservados <span>VALADARES E FUETA ADVOGADOS</span> Desenvolvido por <span>VIRA NEGÓCIOS</span>
+            </p>
           </div>
         </footer>
       </body>
-
     </html>
   );
 }
